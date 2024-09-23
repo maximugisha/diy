ab93c6bfa7c659f65d8c4f1deeffee7df9549e9e
maximugisha <maximugisha@gmail.com>
2024-09-24 01:24:13 +0300

    allowed hosts in .env

--- account/models.py
+++ account/models.py
@@ -29,7 +29,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, related_name='user_profile')
     profile_picture = models.UUIDField(editable=True, default=uuid.uuid4)
     biography = models.TextField(max_length=500, blank=True, null=True)
-    interests = models.ManyToManyField('account.Interest', related_name='interests', blank=True, null=True)
+    interests = models.ManyToManyField('account.Interest', related_name='interests', blank=True)
     role = models.ForeignKey(Role, related_name='user_role', blank=True, null=True, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, related_name='user_organization',
                                      blank=True, null=True, on_delete=models.CASCADE
--- kidsdiy/settings.py
+++ kidsdiy/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import  os
 from pathlib import Path
 from datetime import timedelta
-from decouple import config
+from decouple import Csv, config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-flzxn)a7gmt$$zv6_20mo6h5=a@55#xxym2&dh7r2&6uzrs0z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
+CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE')
 
 # Application definition
 
