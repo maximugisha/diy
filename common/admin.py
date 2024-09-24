1422639bbcdb813de2d77e16c459b85753376368
maximugisha <maximugisha@gmail.com>
2024-09-24 21:50:36 +0300

    image ipl

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from common.models import ImageUpload
+
+
 # Register your models here.
+@admin.register(ImageUpload)
+class ImageUploadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image')
