ece66372ef1cf904bdb7ba6b8d81bddc55d239d0
maximugisha <maximugisha@gmail.com>
2024-09-30 15:51:08 +0300

    add deploy

--- account/signals.py
+++ account/signals.py
@@ -2,9 +2,14 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from account.models import UserProfile
+from decouple import config
+
+
+def get_default_profile_picture():
+    return config('DEFAULT_PROFILE_PICTURE')
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.get_or_create(user=instance, profile_picture="56b442f5-86b0-4aa3-8b04-826d62972c9a")
+        UserProfile.objects.get_or_create(user=instance, profile_picture=get_default_profile_picture)
